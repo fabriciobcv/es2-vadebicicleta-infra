862027935a020332caf7d68e51b2b75fae2fe275
fabriciobcv <fabriciobcvpessoal@gmail.com>
2024-08-31 22:58:20 -0300

    adding two subnets in alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [local.subnet_id]
+  subnets            = [local.subnet_id1, local.subnet_id2]
 
   tags = {
     Name = "es2-vadebicicleta-${local.app_name}"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,5 +4,6 @@ locals{
   app_name = "alb"
   alb_port = 80
   commit = var.commit
-  subnet_id = "subnet-01ad4ba0fb0a6582d"
+  subnet_id1 = "subnet-01ad4ba0fb0a6582d"
+  subnet_id2 = "subnet-0a98f796d4be71c54"
 }
